35c15f13bd1e48664ea3df068c8280cfe57d972b
Carlos <carloscarosaenz@gmail.com>
2025-02-10 21:36:33 -0500

    se mueven archivos a carpeta

---
+++ cloud/garantias.thrift
@@ -0,0 +1,10 @@
+namespace py garantias
+
+struct Garantias {
+    1: i32 id,
+    2: i32 id_venta,
+    3: string estado_garantia,
+    4: string fecha_solicitud,
+    5: string fecha_respuesta,
+    6: string comentario,
+}
